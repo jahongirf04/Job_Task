c149bebe73aca617707a398a4277070b420021e5
Jahongirf04 <jahongirf04@gmail.com>
2023-12-12 11:38:12 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        global1: "#12486B",
+        error_color: 'crimson',
+        success_color: 'green'
+      }
+    },
+  },
+  plugins: [],
+}
+
